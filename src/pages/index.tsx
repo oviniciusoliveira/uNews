9d5f34b80b490bd52a091625a699f3a6fcbec9cc
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-06 14:06:32 -0300

    chore: config external Roboto font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import styles from "../styles/Home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>
-        Hello <span>Next</span>
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>u.news</title>
+      </Head>
+
+      <div>
+        <h1 className={styles.title}>
+          Hello <span>Next</span>
+        </h1>
+      </div>
+    </>
   );
 }
