d1ed238383d7661836d04b2511c61644fa4e2f16
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-06 17:09:33 -0300

    chore: change price data from SSR to SSG

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # u.news
+
+Formas de se obter dados
+- Client-side
+- Server-side
+- Static Site Generation
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KF142J3DdVlGVY5KArfIqwy", {
     expand: ["product"],
   });
@@ -58,5 +58,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24 // 24 hours
   };
 };
