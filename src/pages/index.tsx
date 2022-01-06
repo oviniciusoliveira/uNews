f720e0c9da566e2b48d81e5be72b8c17a4bd19d0
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-06 13:46:27 -0300

    chore: install sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from "../styles/Home.module.scss";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello Next</h1>
+      <h1 className={styles.title}>
+        Hello <span>Next</span>
+      </h1>
     </div>
   );
 }
