1e7c618f21dfa71da9b558fc0011edf30ca7598f
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-06 23:32:32 -0300

    chore: create a new user in faunadb if it doesnt exists on signin; update user email if it already exists in signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { faunaClient } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,29 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await faunaClient.query(
+          q.Let(
+            {
+              match: q.Match(q.Index("user_by_email"), q.Casefold(email)),
+            },
+            q.If(
+              q.Exists(q.Var("match")),
+              q.Get(q.Var("match")),
+              q.Create(q.Collection("users"), { data: { email } })
+            )
+          )
+        );
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
 });
