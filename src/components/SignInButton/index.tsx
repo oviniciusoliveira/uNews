7ce8e703df4ecc66a640d2b4d391279aa800ba58
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-03-14 21:23:07 -0300

    refactor: change SignInButton isUserLoggedIn state to a const

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,12 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useState, useEffect } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const { status, data: session } = useSession();
-
-  useEffect(() => {
-    setIsUserLoggedIn(status === "authenticated");
-  }, [status]);
+  const isUserLoggedIn = status === "authenticated";
 
   return isUserLoggedIn ? (
     <button
